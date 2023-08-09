1cec8a0ee7d9fcfabdfd05bed706d64487c02626
Amy Haywood Dutton <me@amyhaywood.com>
2023-08-09 09:06:27 -0500

    💬 Changed CTA to "Register"

--- web/src/components/SubmitApplication/SubmitApplication.tsx
+++ web/src/components/SubmitApplication/SubmitApplication.tsx
@@ -8,7 +8,7 @@ const SubmitApplication = () => {
       rel="noreferrer"
       className="my-5 block w-full bg-carminePink py-4 text-center font-wide uppercase text-white no-underline hover:bg-sunshade sm:py-5 sm:text-xl"
     >
-      Submit an Application
+      Register to Participate
     </a>
   )
 }
